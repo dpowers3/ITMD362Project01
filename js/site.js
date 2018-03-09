97c9c8cc3420784433306e6fb32ea9e3395ad97e
David Powers <dpowers@hawk.iit.edu>
2018-03-08 21:31:56 -0600

    inserted new validation javascript

--- js/site.js
+++ js/site.js
@@ -1,17 +1,7 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function ($) {
-    $('html').removeClass('nojs');
-    $("html").addClass("hasjs");
-    $("#email").on("focus", function () {
-        $("#input-email label").addClass("active");
-    });
-    $("#email").on("blur", function () {
-        if ($("#email").val().length === 0) {
-            $("#input-email label").removeClass("active");
-        }
-    });
-    $("#alt-box").on("click", function () {
-        $("#age-verification").prop("checked", true);
-        $("#alt-box").html("<p>&#10004;</p>");
-    });
-});
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
